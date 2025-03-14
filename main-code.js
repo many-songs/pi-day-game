d4b3a35b9b27f08401cf1b8a6d7e7b2d467133b0
many-songs <17499@sequoiagrove.org>
2025-03-14 03:29:31 +0000

    added canvas

---
+++ main-code.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("canvas")
+canvas.height=canvas.style.height
+canvas.width=canvas.style.width
+let height=canvas.height
+let width=canvas.width
+let ctx = canvas.getContext("2d")
+ctx.fillRect(0, 0, width, height)
